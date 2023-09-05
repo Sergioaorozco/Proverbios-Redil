c5d17971c6ea54a75b24b76481c366b833b0dfbb
Sergio <sergioa.orozcoo@gmail.com>
2023-09-05 08:51:00 -0500

    Add plyr and functionality

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Proverbio del Día</h1>
+    <p>{{ formattedDate }}</p>
+    <vue-plyr>
+      <audio controls playsinline>
+        <source :src="audioSrc" type="audio/mp3">
+        Your browser does not support the audio element.
+      </audio>
+    </vue-plyr>
+  </div>
+</template>
+
+<script lang="ts">
+const dateInformation = new Date();
+
+export default {
+  data() {
+    return {
+      date: dateInformation,
+      audioSrc:''
+    };
+  },
+  computed: {
+    dailyAudio() {
+      const dayOfMonth = this.date.getDate();
+      this.audioSrc = `public/proverbios/${dayOfMonth}%20Pista.mp3`;
+    },
+    formattedDate() {
+      const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+      };
+      return this.date.toLocaleDateString('es-ES', options as Intl.DateTimeFormatOptions);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
